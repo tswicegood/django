e0c321403cea6e707fbd38eb78b54d8ad7385b72
Jannis Leidel <jannis@leidel.info>
2011-04-23 23:27:59 +0200

    Updated django.db.models.loading to raise a PendingDeprecationWarning.

--- django/db/models/loading.py
+++ django/db/models/loading.py
@@ -1,27 +1,10 @@
-"Utilities for loading models and the modules that contain them."
+import warnings
 
-from django.conf import settings
-from django.core.apps import App, AppCache, cache
-from django.core.exceptions import ImproperlyConfigured
-from django.utils.datastructures import SortedDict
-from django.utils.importlib import import_module
-from django.utils.module_loading import module_has_submodule
+from django.apps import (App, AppCache, cache,
+    find_app, get_apps, get_app, get_models, get_model, register_models,
+    load_app, app_cache_ready)
 
-import imp
-import sys
-import os
-import threading
-
-__all__ = ('get_apps', 'get_app', 'get_models', 'get_model', 'register_models',
-        'load_app', 'app_cache_ready')
-
-# These methods were always module level, so are kept that way for backwards
-# compatibility.
-get_apps = cache.get_apps
-get_app = cache.get_app
-get_app_errors = cache.get_app_errors
-get_models = cache.get_models
-get_model = cache.get_model
-register_models = cache.register_models
-load_app = cache.load_app
-app_cache_ready = cache.app_cache_ready
+warnings.warn(
+    'The utilities in django.db.models.loading have been moved to django.apps. Please update your code accordingly.',
+    PendingDeprecationWarning
+)
