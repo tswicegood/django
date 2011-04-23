2f1faf9a0a190e7d2c9d0d5669facd979543ea1a
Arthur Koziel <arthur@arthurkoziel.com>
2011-04-23 21:18:49 +0200

    remove unneeded imports

--- tests/regressiontests/app_loading/tests.py
+++ tests/regressiontests/app_loading/tests.py
@@ -1,10 +1,8 @@
-import copy
 import os
 import sys
 import time
 
 from django.conf import Settings
-from django.db.models.loading import cache, load_app
 from django.utils.unittest import TestCase
 
 
